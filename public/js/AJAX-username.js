d9c5cac9d6161639b11ced3bfb0dc311d6efac78
MamdouhShawer <shawermamdouh4@gmail.com>
2023-05-30 20:47:46 +0300

    adding files to validate username using ajax

---
+++ public/js/AJAX-username.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#username").on('keyup', function (e) {
+        e.preventDefault();
+        var data = $('#username').val();
+        $.ajax({
+            url: '/user/checkUN',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ UserName: data }),
+            success: function (response) {
+                $('#result').html('UserName is ' + response);
+
+                if (response == 'taken') {
+                    $('#result').css("color", "red");
+                }
+                else {
+                    $('#result').css("color", "green");
+                }
+            },
+            error:function(err){
+
+            }
+        });
+    });
+});
