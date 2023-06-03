35bb4a809e76ad68913f1a8c7195e3d05b83f71a
Nada2063 <120319804+Nada2063@users.noreply.github.com>
2023-06-03 21:56:08 +0200

    display products

--- routers/myStore.js
+++ routers/myStore.js
@@ -1,11 +1,25 @@
 import { Router } from "express";
 
 const router = Router();
+import prod from "../models/products.js"
 
-router.get("/", function (req, res) {
-  console.log("myStore.js: GET /myStore");
+router.get('/', function(req, res, next) {
+  var query = { category: "beds" };
+  Promise.all([
+    prod.find(query)
+    
+  ])
+    .then(([result1]) => {
+      
+      res.render("pages/myStore", { product: result1 , user: (req.session.user === undefined ? "" : req.session.user) });
+    })
+    
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    });
+})
+   
 
-  res.render("pages/myStore", { title: "Couchly | myStore" });
-});
 
-export default router;
+export default router
