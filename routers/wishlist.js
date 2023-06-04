7b678c37de38d643891dd78befbc115df799b629
rana2105525 <125667268+rana2105525@users.noreply.github.com>
2023-06-05 00:41:46 +0300

    admin can't access the user pages

--- routers/wishlist.js
+++ routers/wishlist.js
@@ -3,9 +3,15 @@ import {Router} from 'express';
 const router=Router();
 
 router.get('/',function(req,res){
-    console.log('wishlist.js: GET /wishlist');
+    if (req.session.user !== undefined && req.session.user.type === 'user') {
+        console.log('wishlist.js: GET /wishlist');
 
-    res.render("pages/wishlist",{ user: (req.session.user === undefined ? "" : req.session.user) });
+        res.render("pages/wishlist",{ user: (req.session.user === undefined ? "" : req.session.user) });
+    }
+    else {
+        res.render('pages/err', { err: "You are not user ,  you can't access this page ", user: (req.session.user === undefined ? "" : req.session.user) })
+    }
 });
+
 
 export default router;
