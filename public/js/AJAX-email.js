10058c3252b6538178d77f55986890b6959335f4
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-06-04 18:51:19 +0300

    add email validations

--- public/js/AJAX-email.js
+++ public/js/AJAX-email.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $("#email").on('keyup', function () {
+   $("#email").on('keyup', function () { 
       if ($(this).val().trim() === '') {
         $(this).removeClass('typed').addClass('empty');
       } else {
@@ -7,4 +7,18 @@ $(document).ready(function () {
       }
  
     });
-  });
+   
+  
+
+
+  if($(this).val().trim() === ''||$(this).val().trim()!==/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/ ){
+    $('#resultt').css('color', 'red');
+    $('#resultt').removeClass('typed').addClass('empty');
+     $('#resultt').html('email' + ' is invalid.');
+  } 
+  else{
+    $('#resultt').css('color', 'green');
+    $('#resultt').removeClass('empty').addClass('typed'); 
+    $('#resultt').html('email'+ ' is valid.');
+  }
+  }); 
