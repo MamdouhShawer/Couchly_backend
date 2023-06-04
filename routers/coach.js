f8547e72f06f4e27f31ee54dd1f97135040c08e5
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-06-05 02:55:04 +0300

    fetching couches from database

--- routers/coach.js
+++ routers/coach.js
@@ -1,11 +1,13 @@
 import {Router} from 'express';
+import prod from "../models/products.js";
 
 const router=Router();
 
-router.get('/',function(req,res){
+router.get('/',async(req,res)=>{
     console.log('coach.js: GET /coach');
-
-    res.render("pages/coach",{ user: (req.session.user === undefined ? "" : req.session.user) });
+    const products = await prod.find({ category: 'couches' });
+    console.log(JSON.stringify({ products }))
+    res.render("pages/coach",{ products,user: (req.session.user === undefined ? "" : req.session.user) });
 });
 
 export default router;
