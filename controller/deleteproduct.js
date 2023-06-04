421f00e7018c0ba6c998c721b68bc6f01690708e
Nada2063 <120319804+Nada2063@users.noreply.github.com>
2023-06-05 00:16:37 +0200

    delete product

---
+++ controller/deleteproduct.js
@@ -0,0 +1,15 @@
+import prod from "../models/products.js";
+
+const deleteProd = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    await prod.findByIdAndDelete(id);
+
+    res.status(200).json({ message: "product deleted successfully." });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete product." });
+  }
+};
+
+export default deleteProd;
