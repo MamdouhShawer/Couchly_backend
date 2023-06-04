01b76f5ab35ed0cb9661ec88994d6ffb5379ce9b
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-06-04 20:33:44 +0300

    confirm pass using ajax

--- public/js/AJAX-confirmpass.js
+++ public/js/AJAX-confirmpass.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
-    $("#pass2").on('keyup', function () {
-      if ($(this).val().trim() === '') {
-        $(this).removeClass('typed').addClass('empty');
+    $("#pass2","#pass1").on('keyup', function () {
+      if ($('#pass').val().trim() == $('#pass2').val().trim()) {  
+        $('#pass2').removeClass('empty').addClass('typed');
       } else {
-        $(this).removeClass('empty').addClass('typed');
+       $('#pass2').removeClass('typed').addClass('empty');
       }
- 
+      
     });
   });
