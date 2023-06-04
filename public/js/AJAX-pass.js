f4457acc2aecef9ba6c22a9b91683add0004e4b3
Mayawaleed <125691425+Mayawaleed@users.noreply.github.com>
2023-06-04 19:17:34 +0300

    password validation ajax

--- public/js/AJAX-pass.js
+++ public/js/AJAX-pass.js
@@ -1,9 +1,16 @@
 $(document).ready(function () {
     $("#pass").on('keyup', function () {
-      if ($(this).val().trim() === ''||$(this).val().trim().length<8) {
+        if($(this).val().trim()==''){
+            $(this).removeClass('typed').addClass('empty');
+            $('#result1').html('');
+        }
+      else if ($(this).val().trim().length<8) {
         $(this).removeClass('typed').addClass('empty');
+        $('#result1').css('color', 'red');
+        $('#result1').html('small password');
       } else {
         $(this).removeClass('empty').addClass('typed');
+        $('#result1').html('');
       }
  
     });
