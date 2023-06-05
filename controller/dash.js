9726d2bc2a2b047e1e4deb09e1894613ab7a1d14
Ahmedhossam26 <125680149+Ahmedhossam26@users.noreply.github.com>
2023-06-05 14:33:56 +0300

    solving an error in dahsboard

--- controller/dash.js
+++ controller/dash.js
@@ -2,13 +2,24 @@
 
 import Users from "../models/user.js";
 import check from "../models/chckout.js";
-import Prod from "../models/products.js";
+import prod from "../models/products.js";
+
+const TotalPrice = prod
+  .aggregate([{ $group: { _id: null, total: { $sum: "$price" } } }])
+  .then((result) => {
+    return result[0].total;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
 
 export async function showDashboard(req, res) {
   try {
     const userCount = await Users.countDocuments();
     const checkCount = await check.countDocuments();
-    const Prices = await Prod.TotalPrice;
+    const Prices = TotalPrice;
+    const id = req.params.id;
+    await prod.find(id);
     res.render("pages/dashboard", {
       title: "Couchly | dashboard",
       showDashboard: userCount,
@@ -20,3 +31,5 @@ export async function showDashboard(req, res) {
     res.status(500).send("Internal server error");
   }
 }
+
+export default TotalPrice;
