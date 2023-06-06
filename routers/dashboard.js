cd79b47339f070fbfd905b8c8c472f891c9286a8
Nada2063 <120319804+Nada2063@users.noreply.github.com>
2023-06-06 23:14:46 +0200

    Recent order search

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { showDashboard } from "../controller/dash.js";
+import searchorder from "../controller/recentorderSearch.js";
 
 const router = Router();
 
 router.get("/", showDashboard);
+router.get("/search",searchorder);
 
 export default router;
