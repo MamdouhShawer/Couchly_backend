f5d05d364e6efbd411eda6e4b4ef7f281abff559
Nada2063 <120319804+Nada2063@users.noreply.github.com>
2023-06-06 15:01:36 +0200

    ubdate in add product

--- routers/products_route.js
+++ routers/products_route.js
@@ -2,11 +2,11 @@ import {Router} from 'express';
 
 import prodform from '../controller/addProd.js';
 const router=Router();
-router.post('/',prodform)
+
 
 /* GET /about/test page. */
 router.get('/', function(req, res) {
     res.render('pages/addProduct',{title:'Add Product'});
   });
-
+router.post('/',prodform)
 export default router;
