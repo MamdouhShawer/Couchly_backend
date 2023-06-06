7d9524a9104f17fe7713325b6424e6a77e35dd69
rana2105525 <125667268+rana2105525@users.noreply.github.com>
2023-06-06 22:30:47 +0300

    error

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -3,15 +3,6 @@ import { showDashboard } from "../controller/dash.js";
 
 const router = Router();
 
-router.get('/',function(req,res){
-    if (req.session.user !== undefined && req.session.user.type === 'admin') {
-        console.log('dashboard.js: GET /dashboard');
-
-        res.render("pages/dashboard",{ user: (req.session.user === undefined ? "" : req.session.user) });
-    }
-    else {
-        res.render('pages/err', { err: "You are not an admin ,  you can't access this page ", user: (req.session.user === undefined ? "" : req.session.user) })
-    }
-});
+router.get("/", showDashboard);
 
 export default router;
