18fcfdcf915ca112747b710f8a0b900ab7f681a5
rana2105525 <125667268+rana2105525@users.noreply.github.com>
2023-06-06 22:02:24 +0300

    user or undefined accounts can't access the admin pages

--- routers/dashboard.js
+++ routers/dashboard.js
@@ -3,6 +3,15 @@ import { showDashboard } from "../controller/dash.js";
 
 const router = Router();
 
-router.get("/", showDashboard);
+router.get('/',function(req,res){
+    if (req.session.user !== undefined && req.session.user.type === 'admin') {
+        console.log('dashboard.js: GET /dashboard');
+
+        res.render("pages/dashboard",{ user: (req.session.user === undefined ? "" : req.session.user) });
+    }
+    else {
+        res.render('pages/err', { err: "You are not an admin ,  you can't access this page ", user: (req.session.user === undefined ? "" : req.session.user) })
+    }
+});
 
 export default router;
