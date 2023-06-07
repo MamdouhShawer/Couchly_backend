f7a22f895b891dd0f85042532659b01dc04c9792
Ahmedhossam26 <125680149+Ahmedhossam26@users.noreply.github.com>
2023-06-07 13:21:00 +0300

    adding translator

--- controller/editproduct.js
+++ controller/editproduct.js
@@ -1,21 +1,25 @@
 import prod from "../models/products.js";
 
-
 const editProduct = async (req, res) => {
   const id = req.params.id;
 
-        prod.findByIdAndUpdate( ireq.session.user._id, { name:req.body.ProductName,quantity: req.body.quantity, price: req.body.price,description: req.body.description  })
-        .then(result => {
-          req.session.user.name=req.body.ProductName;
-          req.session.user.quantity=req.body.quantity;
-          req.session.user.price=req.body.price;
-          req.session.user.description=req.body.description;
-          res.render("editProduct", { id: id });
-        })
-        .catch(err => {
-            console.log(err);
-        });
-  };
-  
-  
-  export default editProduct;
+  prod
+    .findByIdAndUpdate(id, {
+      name: req.body.ProductName,
+      quantity: req.body.quantity,
+      price: req.body.price,
+      description: req.body.description,
+    })
+    .then((result) => {
+      req.session.user.name = req.body.ProductName;
+      req.session.user.quantity = req.body.quantity;
+      req.session.user.price = req.body.price;
+      req.session.user.description = req.body.description;
+      res.render("editProduct", { id: id });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export default editProduct;
